051dee415fa34a50d1ff4d72d579eaeb67049882
Houssein Ben Amor <houssein@rockets-MacBook-Pro-10.local>
2014-09-29 15:05:10 +0200

    Initial Commit

---
+++ iosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosTest
+//
+//  Created by Houssein Ben Amor on 29/09/14.
+//  Copyright (c) 2014 Houssein Ben Amor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iosTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iosTest
+//
+//  Created by Houssein Ben Amor on 29/09/14.
+//  Copyright (c) 2014 Houssein Ben Amor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
